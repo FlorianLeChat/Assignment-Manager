87adf54dcecedb18fb69e0cf8085d235105993e3
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2022-09-15 11:49:52 +0200

    Ajout d'un composant enfant
    Co-Authored-By: Agares1990 <52073076+Agares1990@users.noreply.github.com>

---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: [ './assignments.component.css' ]
+} )
+export class AssignmentsComponent implements OnInit
+{
+
+  "nomProf": "Test";
+
+  constructor() { }
+
+  ngOnInit(): void
+  {
+  }
+
+}
